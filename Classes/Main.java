43bc2359059804fc703fbaf3e4c2f61efa7cf1f3
Stefany Souza <sttefanyss14@gmail.com>
2025-04-24 15:48:04 -0300

    Create Main.java

---
+++ Classes/Main.java
@@ -0,0 +1,14 @@
+package Classes;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+class Carro {
+    public Carro() {
+        System.out.println("Carro criado!");
+    }
+   
+}
